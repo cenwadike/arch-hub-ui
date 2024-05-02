9a0a9eb6a0190769fa6006398a5f126aeb981f27
cenwadike <chinedunwadike04@gmail.com>
2024-05-02 17:52:25 +0100

    navbar

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,28 +1,18 @@
-import { useState } from 'react';
-import { Sidebar, Menu, MenuItem,  } from 'react-pro-sidebar';
+import { MdHomeFilled } from "react-icons/md";
+import { LiaMoneyCheckAltSolid } from "react-icons/lia";
+import { TbStatusChange } from "react-icons/tb";
+import { VscOpenPreview } from "react-icons/vsc";
+import SidebarIcon from '../SideBarIcon';
 
 
 export default function NavBar() {
-    const [collapsed, setCollapsed] = useState(false);
-
     return (
         <>
-           <div style={{ display: 'flex', height: '100%', minHeight: '400px' }}>
-                <Sidebar collapsed={collapsed} transitionDuration={1000}>
-                    <Menu>
-                    <MenuItem> Profile</MenuItem>
-                    <MenuItem> Invoices</MenuItem>
-                    <MenuItem> Status</MenuItem>
-                    <MenuItem> Reviews</MenuItem>
-                    </Menu>
-                </Sidebar>
-                <main style={{ padding: 10 }}>
-                    <div>
-                    <button className="sb-button" onClick={() => setCollapsed(!collapsed)}>
-                        Collapse
-                    </button>
-                    </div>
-                </main>
+            <div className='fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900 text-secondary shadow'>
+                <SidebarIcon icon={<MdHomeFilled size='28' />} text={"profile"} />
+                <SidebarIcon icon={<LiaMoneyCheckAltSolid size='28' />} text={"invoices"} />
+                <SidebarIcon icon={<TbStatusChange size='28' />} text={"status"} />
+                <SidebarIcon icon={<VscOpenPreview size='28' />} text={"reviews"}/>
             </div>
         </>
     )
