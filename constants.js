f51e142d5cdc036ce964067f3c936c3c5f26f46a
cenwadike <chinedunwadike04@gmail.com>
2024-05-23 15:27:14 +0100

    update address

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const CONTRACT_TESTNET_ADDRESS = "archway1r06hv75sg2gqle757kkdndenvq9v2e9ne50w6j0qmvjx5nvcaqnq279s4f";
+export const CONTRACT_TESTNET_ADDRESS = "archway1eukrde94x8ns4pgzvwy2w68y6ntrdr0vtnk9mydh2jler438456q9rmm3u";
 export const IPFS_ENDPOINT = "https://ipfs.infura.io:5001"
 export const INFURA_API_KEY = "25YgFjDoa4wN50egzBDz5E9m9v9"
 export const INFURA_API_SECRET = "c3b4650e90e9326e80e2918fd3dc6d35"
